9ae339dc83ac4148042a10752c4093478fb77b86
carloslopllo <carlos.lopez.llorente@alumnos.upm.es>
2024-10-25 00:52:59 +0200

    annotations : Mojo, LifecyclePhase

--- src/main/java/com/github/carloslopllo/MyMojo.java
+++ src/main/java/com/github/carloslopllo/MyMojo.java
@@ -2,7 +2,10 @@ package com.github.carloslopllo;
 
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
 
+@Mojo(name="pluginTestSmell", defaultPhase=LifecyclePhase.TEST)
 public class MyMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
 
